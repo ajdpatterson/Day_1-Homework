e98751fb0dbda4872e8534eba063769eb161e54f
Andrew Patterson <saladin.zero@gmail.com>
2022-11-07 16:09:29 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+meal_1 = "Steak surprise"
+meal_2 = "Grapefruit Supreme"
+meal_1_cost = 10.99
+meal_2_cost = 14.09
+
+meal_selection_text = meal_1 + " and " + meal_2
+meal_cost_sum = meal_1_cost + meal_2_cost
+print("You have chosen " + meal_selection_text)
+print(f"Please pay £{meal_cost_sum}")
