2aebfdecdff4eccf1d7be6f5709888cfa2a236a7
Andrew Patterson <saladin.zero@gmail.com>
2022-11-07 16:19:05 +0000

    Tinkering for fun

--- precourse_recap.py
+++ precourse_recap.py
@@ -6,4 +6,5 @@ meal_2_cost = 14.09
 meal_selection_text = meal_1 + " and " + meal_2
 meal_cost_sum = meal_1_cost + meal_2_cost
 print("You have chosen " + meal_selection_text)
-print(f"Please pay £{meal_cost_sum}")
+print(f"25% discount applied! You have saved £{meal_cost_sum * 0.25}")
+print(f"Please pay £{meal_cost_sum * 0.75}")
